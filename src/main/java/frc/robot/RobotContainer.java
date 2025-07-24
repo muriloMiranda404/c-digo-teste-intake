0420f5d6631fe75dc2a19059c3448b51bd79aad9
muriloMiranda404 <murilomiranda404@gmail.com>
2025-07-24 20:15:50 -0300

    atualização

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -17,8 +17,10 @@ import java.nio.file.FileSystem;
 import com.pathplanner.lib.commands.PathPlannerAuto;
 
 import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
@@ -26,19 +28,18 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 public class RobotContainer {
 
-  public final CommandXboxController DriveController = new CommandXboxController(Controllers.DRIVER_CONTROLLER);
+  public final XboxController DriveController = new XboxController(Controllers.DRIVER_CONTROLLER);
   public final XboxController intakeController = new XboxController(Controllers.INTAKE_CONTROLLER);
 
-  public static SwerveSubsystem swerve = new SwerveSubsystem(new File(Filesystem.getDeployDirectory(), "swerve"));
-  public static final IntakeSubsystem intake = IntakeSubsystem.getInstance();
+  public final SwerveSubsystem swerve = new SwerveSubsystem(new File(Filesystem.getDeployDirectory(), "swerve"));
+  public final IntakeSubsystem intake = IntakeSubsystem.getInstance();
 
   public RobotContainer() {
 
     swerve.setDefaultCommand(swerve.driveCommand(
-      () -> MathUtil.applyDeadband(DriveController.getLeftY(), 0.0),
-      () -> MathUtil.applyDeadband(DriveController.getLeftX(), 0.,0),
-      () -> MathUtil.applyDeadband(DriveController.getRightX(), 0.0)
-    ));
+      () -> MathUtil.applyDeadband(DriveController.getLeftY(), 0),
+      () -> MathUtil.applyDeadband(DriveController.getLeftX(), 0),
+      () -> MathUtil.applyDeadband(DriveController.getRightX(), 0)));
 
     configureBindings();
   }
@@ -46,15 +47,34 @@ public class RobotContainer {
  
   private void configureBindings() {
 
-    new JoystickButton(intakeController, 10).whileTrue(new IntakeSpeed(intake, 1.0));
-    new JoystickButton(intakeController, 9).whileTrue(new IntakeSpeed(intake, -1.0));
-  
-    new JoystickButton(intakeController, 1).onTrue(new IntakePosition(intake, 231));
+    new JoystickButton(intakeController, 10).whileTrue(new IntakeSpeed(intake, 0.7));
+    new JoystickButton(intakeController, 9).whileTrue(new IntakeSpeed(intake, -0.7));
     
+    new JoystickButton(intakeController, 1).onTrue(new IntakePosition(intake, 120));
   }
 
 
   public Command getAutonomousCommand() {
     return new PathPlannerAuto(Autonomous.AUTO);
+  }
+
+  public double setChoose(int choose){
+
+    int inverter = DriverStation.getAlliance().get() == Alliance.Red ? -1 : 1;
+    double marcha = 0.7;
+
+    if(DriveController.getRightBumperButton()){
+      marcha = 1.0;
+    } else if(DriveController.getLeftBumperButton()){
+      marcha = 0.2;
+    } else {
+      marcha = 0.7;
+    }
+
+    if(choose == 1) return DriveController.getLeftY() * marcha * inverter;
+    else if(choose == 2) return DriveController.getLeftX() * marcha * inverter;
+    else if(choose == 3) return DriveController.getRightX() * marcha;
+
+    return choose;
   }
 }
